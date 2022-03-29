e5690715b198e3f5d2059e6b469615ab006428ba
TwoDollarsRamen <s211427@students.aie.edu.au>
2022-03-29 15:16:57 +1100

    Shadows!

--- graphics/src/renderer.c
+++ graphics/src/renderer.c
@@ -85,6 +85,19 @@ static m4f get_light_matrix(struct light* light, struct aabb scene) {
 
 	scene = transform_aabb(scene, view);
 
+	float z_mul = 10.0f;
+	if (scene.min.z < 0.0f) {
+		scene.min.z *= z_mul;
+	} else {
+		scene.min.z /= z_mul;
+	}
+
+	if (scene.max.z < 0.0f) {
+		scene.max.z /= z_mul;
+	} else {
+		scene.max.z *= z_mul;
+	}
+
 	m4f proj = m4f_orth(
 		scene.min.x, scene.max.x,
 		scene.min.y, scene.max.y,
